4021b33525411f055793055543aa8538eef657b1
bcklosiu <unai_lorenzo@ehu.es>
2015-03-17 16:37:49 +0100

    Cambios en fitfcn_FCS_3DTauD para ajuste global

--- fitfcn/fitfcn_FCS_3DTauD.m
+++ fitfcn/fitfcn_FCS_3DTauD.m
@@ -1,5 +1,5 @@
-function G=fitfcn_FCS_3DTauD (param, tdata)
-
+function G=fitfcn_FCS_3DTauD (param, indParam, x)
+           
 % G=fitfcn_FCS_3DTauD (param, tdata)
 % Función de autocorrelación con un modelo de difusion 3D single photon 
 %  G = (1/N)*((1+tdata/tauD).^-1).*((1+tdata/(tauD*SP^2)).^-0.5)
@@ -16,7 +16,13 @@ function G=fitfcn_FCS_3DTauD (param, tdata)
 % param(3)=SP
 %
 % jri 4Feb15
+% Unai 6Mar15, modificado para ajuste global
 
+tdata=x(:,1);
+dsid=x(:,2); %Indica el nº de curva de cada set de datos
+N=param(indParam(:,1));
+tauD=param(indParam(:,2));
+s=param(indParam(:,3));
 
- G = ((1+tdata/(param(2)*1E-3)).^-1).*((1+tdata/(param(2)*1E-3*param(3)^2)).^-0.5)/param(1);
+G = ((1+tdata./(tauD(dsid)*1E-3)).^-1).*((1+tdata./(tauD(dsid).*1E-3.*s(dsid).^2)).^-0.5)./N(dsid);
  
